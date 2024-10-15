66e1a1d6ce37362b0e599801ad5e9039df48e188
Saloni Tilekar <sbtilekar@csuchico.edu>
2024-10-14 21:13:50 -0700

    environment setup

---
+++ sanctions-screening/src/main/java/com/stilekar/sanctions_screening/SanctionsScreeningApplication.java
@@ -0,0 +1,13 @@
+package com.stilekar.sanctions_screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanctionsScreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanctionsScreeningApplication.class, args);
+	}
+
+}
